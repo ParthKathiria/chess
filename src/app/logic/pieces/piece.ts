e34c935d6ce736e3d816e8106b39593d017c135b
parth.kathiria <parth.kathiria169@gmail.com>
2024-09-08 14:51:08 -0400

    implemented piece structure

---
+++ src/app/logic/pieces/piece.ts
@@ -0,0 +1,18 @@
+import { Color, Coords, FENChar } from "../models";
+
+export abstract class Piece {
+    protected abstract _FENCHAR: FENChar;
+    protected abstract _directions: Coords[];
+
+    constructor(private _color: Color){}
+
+    public get FENChar(): FENChar {
+        return this._FENCHAR;
+    }
+    public get directions(): Coords[] {
+        return this._directions;
+    }
+    public get color(): Color {
+        return this.color;
+    }
+}
